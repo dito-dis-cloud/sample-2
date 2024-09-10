7620ed078be64cf26d955ee5e862ffd9788ecd6f
dito-dis-cloud <110221148+dito-dis-cloud@users.noreply.github.com>
2024-09-10 17:00:26 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -54,9 +54,7 @@ def result_post():
     price = request.form["price"]
 
     # オブジェクトの型で振る舞いを変える
-    if type(price) == int:
-        pass
-    else :
+    if type(price) != int:
         msg = print(f'{price}の型はintです')
         return render_template('index.html', msg = msg)
 
